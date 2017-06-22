1dd11f05a6665d91e6437ec5911a02d2ed6dfbff
Gonzalez <luciano.j.gonzalez@accenturefederal.com>
2017-06-22 11:21:20 -0400

    Added ability to redirect to intended page after login or admin view by default

--- TASHealthCheck/src/main/java/com/tas/healthcheck/security/customAuthenticationSuccessHandler.java
+++ TASHealthCheck/src/main/java/com/tas/healthcheck/security/customAuthenticationSuccessHandler.java
@@ -2,7 +2,8 @@ package com.tas.healthcheck.security;
 
 import java.io.IOException;
 import java.util.Set;
- 
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +16,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.SavedRequest;
  
 public class customAuthenticationSuccessHandler implements
 		AuthenticationSuccessHandler {
@@ -25,23 +27,41 @@ public class customAuthenticationSuccessHandler implements
 	public void onAuthenticationSuccess(HttpServletRequest request,
 			HttpServletResponse response, Authentication authentication) throws IOException,
 			ServletException {
+		System.out.println("In CustomAuth");
+
 		HttpSession session = request.getSession();
 		
-		System.out.println("In CustomAuth");
+ 		SavedRequest savedRequest = (SavedRequest) session.getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+ 		
+ 		String redirectUri = "/applications";
+ 		
+ 		if(savedRequest != null){
+ 			String urlToRedirect = savedRequest.getRedirectUrl();
+ 		
+ 			String loginUrl = request.getRequestURL().toString();
+ 			loginUrl = loginUrl.substring(0, loginUrl.length() - 6);
+ 		
+ 			redirectUri = urlToRedirect.replace(loginUrl, "");
+ 			
+ 			if(redirectUri.equals("/login")){
+ 				redirectUri = "/applications";
+ 			}
+ 		}
+ 		
 		/*Set some session variables*/
 		User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  
         session.setAttribute("uname", authUser.getUsername());  
         session.setAttribute("authorities", authentication.getAuthorities()); 
         
         /*Set target URL to redirect*/
-		String targetUrl = determineTargetUrl(authentication); 
+		String targetUrl = determineTargetUrl(authentication, redirectUri); 
         redirectStrategy.sendRedirect(request, response, targetUrl);
 	}
  
-	protected String determineTargetUrl(Authentication authentication) {
+	protected String determineTargetUrl(Authentication authentication, String successfulRedirectUri) {
         Set<String> authorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (authorities.contains("ROLE_ADMIN")) {
-        	return "/";
+        	return successfulRedirectUri;
         } else {
             throw new IllegalStateException();
         }
--- TASHealthCheck/src/main/webapp/WEB-INF/views/login.jsp
+++ TASHealthCheck/src/main/webapp/WEB-INF/views/login.jsp
@@ -5,7 +5,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Agile Signup Login</title>
+<title>TAS Login</title>
 <link href="<c:url value="/resources/css/bootstrap.min.css" />" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
